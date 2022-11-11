06c3a28db398ec850528c71560aaf0113509f933
Remi Chuet <e.rchuet@etu.cvm.qc.ca>
2022-11-10 21:59:50 -0500

    modification du mouvement de l'enemy

--- human.cpp
+++ human.cpp
@@ -72,11 +72,59 @@ void Human::movementHumanXY(char firstDirection, char secondDirection)
 	}
 }
 
+
+void Human::CalculPossibilityDirection(int playerX, int playerY, int wallX, int wallTopY, int wallDownY)
+{
+	int lenghtPath1 = 0;
+	int lenghtPath2 = 0;
+	char firstDirection = ' ';
+	char secondDirection = ' ';
+	int value1 = 0;
+	int value2 = 0;
+
+	/*
+	if (this->positionY > wallTopY)
+		firstDirection = 'w';
+	else
+		firstDirection = 's';
+	if (this->positionX < playerX)
+		secondDirection = 'd';
+	else
+		secondDirection = 'a';
+	*/
+
+	// calcul pour droit et gauche
+	value1 = pow(this->positionX - wallX, 2);
+
+// calcul pour monter
+	if (this->positionY > wallTopY - 1)						// 'w'	-1 pour grosseur du joueur
+		value2 = pow(this->positionY - posWallTopY, 2);
+	// calcul si plus haut
+	else
+		value2 = 0;
+	lenghtPath1 = sqrt(value1 + value2);				// hypothénuse
+
+	// calculer le segment du mur au joueur
+
+// calcul pour descendre
+	if (this->positionY < wallDownY + 1)
+		value2 = pow(this->positionY - posWallDownY, 2);
+	else
+		value2 = 0;
+	lenghtPath2 = sqrt(value1 + value2);				// hypothénuse
+	// calculer le segment du mur au joueur
+
+}
+
+
 void Human::ChooseDirection(int playerX, int playerY, int wallX, int wallTopY, int wallDownY)
 {
 	char firstDirection = ' ';
 	bool secondBeAble = true;
 	char secondDirection = ' ';
+	
+	// Calculer le chemin le plus rapide
+	// partir dans la direction
 
 	for (int i = 0; i <= 1; i++)
 	{
